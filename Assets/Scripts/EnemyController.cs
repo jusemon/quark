115cfaafe13ea86f4cbc794ddcb736259eb4bc71
Juan Sebastián Montoya <JuanSMM@outlook.com>
2019-11-21 14:35:59 -0500

    Added enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -14,12 +14,13 @@ public class EnemyController : BaseShotController, ICanExplodeController
 
     void FixedUpdate()
     {
-        // Get the new position of our enemy . On X, move left and right; on Y Slowly get down.
-        var x = boundX * Mathf.Sin(Time.deltaTime * speedX);
+        // Get the new position of our Enemy. On X, move left and right; on Y slowly get down. 
+        var x = boundX * Mathf.Sin(Time.fixedTime * Time.deltaTime * speedX);
+
         var y = transform.position.y + Time.deltaTime * speedY;
 
         //Set the position of our character through the RigidBody2D component (since we are using physics) 
-        rigidBody.MovePosition(new Vector2(x, transform.position.y));
+        rigidBody.MovePosition(new Vector2(x, y));
 
         // Check if the player can shoot since last time the spaceship has fired
         if (Time.time - lastTimeShot > reloadTime)
