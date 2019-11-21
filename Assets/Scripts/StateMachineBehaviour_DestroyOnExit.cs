d8dc71a0962db6e4ad8a2f76a56500b1f0f4dc1d
Juan Sebastián Montoya <JuanSMM@outlook.com>
2019-11-21 10:03:09 -0500

    Added Fonts, Sprites and Animations

---
+++ Assets/Scripts/StateMachineBehaviour_DestroyOnExit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// State Machine Behaviour Destroy On Exit class. 
+/// </summary>
+public class StateMachineBehaviour_DestroyOnExit : StateMachineBehaviour
+{
+    /// <summary>
+    /// Called when [state exit].
+    /// </summary>
+    /// <param name="animator">The animator.</param>
+    /// <param name="stateInfo">The state information.</param>
+    /// <param name="layerIndex">Index of the layer.</param>
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Destroy(animator.gameObject);
+    }
+}
